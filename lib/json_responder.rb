b2fe0e90cd1ade783f4e8b3d87eece02cd0e8d1e
Michael Pope <mpope.cr@gmail.com>
2015-01-28 19:28:00 -0500

    Add JsonResponder

---
+++ lib/json_responder.rb
@@ -0,0 +1,39 @@
+require "net/http"
+
+class JsonResponder
+  attr_reader :json,
+              :request,
+              :uri
+
+  def cache_json
+    dir       = "./lib/json/"
+    name      = uri.split("").select { |i| i.match(/(\d|\w)/) }.join
+    extension = ".json"
+    path      = dir + name + extension
+    File.write(path, json)
+  end
+
+  def get_and_set_json
+    json  = Net::HTTP.get(URI(uri))
+    @json = json
+  end
+
+  def run(request)
+    @request = request
+
+    set_uri
+    get_and_set_json
+    cache_json
+    set_response
+  end
+
+  def set_response
+    request[:response] = json
+  end
+
+  def set_uri
+    uri        = request[:env]["REQUEST_URI"]
+    extra_bits = ENV["TEST_SERVER_IP"] + "/"
+    @uri = uri.gsub(extra_bits, "")
+  end
+end
--- lib/mock_api.rb
+++ lib/mock_api.rb
@@ -1,9 +1,19 @@
 class MockApi
+  attr_reader :json_responder,
+              :request
+
+  def initialize
+    @json_responder = JsonResponder.new
+  end
+
   def call(env)
-    [
-      200,
-      {},
-      ["I am a mock api"]
+    @request = { env: env }
+
+    json_responder.run(request)
+
+    [ 200,
+      { "Content-Type" => "application/json" },
+      [ request[:response] ]
     ]
   end
 end
